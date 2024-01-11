d2924009dcc80cd9ba940551e6aea7737ca7ab5f
Connor <connordbrown@gmail.com>
2024-01-11 16:07:41 -0700

    add About to pages

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// display description of application and any other useful information
+function About() {
+    return (
+        <div>
+            <h2>About This App</h2>
+            <p>Nothing here yet. Check back soon!</p>
+        </div>
+    )
+}
+
+export default About;
