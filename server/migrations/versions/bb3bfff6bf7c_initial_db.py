63c0c75aea1c6e16966aca51915f44c87ffb3c11
Connor <connordbrown@gmail.com>
2024-01-11 15:20:33 -0700

    initial commit

---
+++ server/migrations/versions/bb3bfff6bf7c_initial_db.py
@@ -0,0 +1,24 @@
+"""initial db
+
+Revision ID: bb3bfff6bf7c
+Revises: 
+Create Date: 2024-01-11 15:20:09.294382
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'bb3bfff6bf7c'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
