a4089f80ee92438e5bfaca4e59d0ddec102539a3
Connor <connordbrown@gmail.com>
2024-01-11 16:44:56 -0700

    add NavBar to components and NavBar.css

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+// enable links to other pages and .active styling
+import { NavLink } from 'react-router-dom';
+// styling for NavBar
+import './styling/NavBar.css';
+
+// create nav bar multiple endpoints
+function NavBar() {
+    return (
+        <div className='nav-container'>
+            <nav className='navbar'>
+                <NavLink to='/'>Home</NavLink>
+                <NavLink to='/about'>About</NavLink>
+            </nav>
+        </div>
+    )
+}
+
+export default NavBar;
