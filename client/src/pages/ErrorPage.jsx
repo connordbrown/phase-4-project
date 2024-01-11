f96aca40830829ce277ebcd60e49073a743aaa1f
Connor <connordbrown@gmail.com>
2024-01-11 16:05:20 -0700

    add ErrorPage to pages

---
+++ client/src/pages/ErrorPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// display error message if user attempts to reach nonexistent endpoint
+function ErrorPage() {
+    return <h1>404 Error: Page Not Found!</h1>
+}
+
+export default ErrorPage;
