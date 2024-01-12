1c51a4a0b2cb7326664920eab61d03e5bf4ca2cb
Connor <connordbrown@gmail.com>
2024-01-11 17:13:35 -0700

    configure initial client-side routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import NavBar from './components/NavBar.jsx'
+import NavBar from './components/NavBar'
 import Home from './pages/Home'
-import About from './pages/Home'
+import About from './pages/About'
 import ErrorPage from './pages/ErrorPage'
 
 function App() {
@@ -12,7 +12,9 @@ function App() {
     <BrowserRouter>
       <NavBar />
       <Routes>
-
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='*' element={<ErrorPage />} />
       </Routes>
       <h1>Vite + React</h1>
     </BrowserRouter>
