051062a54c90402a3ee359ea8e88bb750feefd6d
Connor <connordbrown@gmail.com>
2024-01-11 20:25:35 -0700

    create initial home page

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -4,7 +4,17 @@ function Home() {
 
     return (
         <div>
-            <h1>Welcome to the Home page!</h1>
+            <h2>Posts</h2>
+            <ul>
+                {/* Posts go here! Include link to see post with comments */}
+            </ul>
+            <hr />
+            <h2>Users</h2>
+            <ul>
+                {/* Users go here! Include links to see comments/posts */}
+            </ul>
+            <hr />
+            {/* User signup goes here! */}
         </div>
     )
 }
