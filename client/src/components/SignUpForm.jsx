a45016a971f40bf9d236d26492d4ab5e678cb490
Connor <connordbrown@gmail.com>
2024-01-12 20:47:11 -0700

    more SignUp styling

--- client/src/components/SignUpForm.jsx
+++ client/src/components/SignUpForm.jsx
@@ -9,8 +9,8 @@ function SignUpForm() {
 
     const formSchema = yup.object().shape({
         name: yup.string().required("Must enter a name").max(15),
-        age: yup.number().positive().integer().required("Must enter age").typeError("Please enter an integer").max(125),
-        email: yup.string().email("Invalid email").required("Must enter email"),
+        age: yup.number().positive().integer().required("Must enter an age").typeError("Please enter an integer").max(125),
+        email: yup.string().email("Invalid email").required("Must enter an email"),
         password: yup.string().required("Must enter a password")
     })
 
@@ -22,64 +22,71 @@ function SignUpForm() {
             password: "",
         },
         validationSchema: formSchema,
+        onSubmit: (values) => {
+            // Handle form submission
+            console.log(values);
+          },   
     })
 
     return (
         <div>
             <h2>User Sign Up Form</h2>
-            <form onSubmit={formik.handleSubmit} style={{ margin: '30px'}}>
-                <div className='form-inputs'>
-                    <label htmlFor='name'>Name:</label>
-                    <br />
-                    <input
-                        id='name'
-                        name='name'
-                        placeholder='user name'
-                        onChange={formik.handleChange}
-                        value={formik.values.name}
-                    />
-                    <p style={{ color: 'red' }}> {formik.errors.name}</p>
-                </div>
-                <div className='form-inputs'>
-                    <label htmlFor='age'>Age:</label>
-                    <br />
-                    <input
-                        id='age'
-                        name='age'
-                        placeholder='user age'
-                        onChange={formik.handleChange}
-                        value={formik.values.age}
-                    />
-                    <p style={{ color: 'red' }}> {formik.errors.age}</p>
-                </div>
-                <div className='form-inputs'>
-                    <label htmlFor='email'>Email:</label>
-                    <br />
-                    <input
-                        id='email'
-                        name='email'
-                        placeholder='user email'
-                        onChange={formik.handleChange}
-                        value={formik.values.email}
-                    />
-                    <p style={{ color: 'red' }}> {formik.errors.email}</p>
-                </div>
-                <div className='form-inputs'>
-                    <label htmlFor='password'>Password:</label>
-                    <br />
-                    <input
-                        id='password'
-                        name='password'
-                        placeholder='user password'
-                        onChange={formik.handleChange}
-                        value={formik.values.password}
-                    />
-                    <p style={{ color: 'red' }}> {formik.errors.password}</p>
-                </div>
-                <div id='button'>
-                    <button type='submit'>Submit</button>
-                </div>
-            </form>
+            <div className='form-container'>
+                <form onSubmit={formik.handleSubmit} style={{ margin: '30px'}}>
+                    <div className='form-inputs'>
+                        <label htmlFor='name'>Name:</label>
+                        <br />
+                        <input
+                            id='name'
+                            name='name'
+                            placeholder='user name'
+                            onChange={formik.handleChange}
+                            value={formik.values.name}
+                        />
+                        <p>{formik.errors.name}</p>
+                    </div>
+                    <div className='form-inputs'>
+                        <label htmlFor='age'>Age:</label>
+                        <br />
+                        <input
+                            id='age'
+                            name='age'
+                            placeholder='user age'
+                            onChange={formik.handleChange}
+                            value={formik.values.age}
+                        />
+                        <p>{formik.errors.age}</p>
+                    </div>
+                    <div className='form-inputs'>
+                        <label htmlFor='email'>Email:</label>
+                        <br />
+                        <input
+                            id='email'
+                            name='email'
+                            placeholder='user email'
+                            onChange={formik.handleChange}
+                            value={formik.values.email}
+                        />
+                        <p>{formik.errors.email}</p>
+                    </div>
+                    <div className='form-inputs'>
+                        <label htmlFor='password'>Password:</label>
+                        <br />
+                        <input
+                            id='password'
+                            name='password'
+                            type='password'
+                            placeholder='user password'
+                            onChange={formik.handleChange}
+                            value={formik.values.password}
+                        />
+                        <p>{formik.errors.password}</p>
+                    </div>
+                    <div id='button'>
+                        <button type='submit'>Submit</button>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
