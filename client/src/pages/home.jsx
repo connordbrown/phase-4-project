7660f6177bc5d271ab0ae40b9d1306681332fa0d
Connor <connordbrown@gmail.com>
2024-01-14 15:48:48 -0700

    display Users on Home page

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,23 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SignUpForm from '../components/SignUpForm';
 
 function Home() {
+  const [users, setUsers] = useState([])
 
-    return (
-        <div>
-            <h2>Posts</h2>
-            <ul>
-                {/* Posts go here! Include link to see post with comments */}
-            </ul>
-            <hr />
-            <h2>Users</h2>
-            <ul>
-                {/* Users go here! Include links to see comments/posts by user */}
-            </ul>
-            <hr />
-            <SignUpForm />
-        </div>
-    )
+  useEffect(() => {
+    fetch("/api/users")
+    .then(response => response.json())
+    .then(data => setUsers(data))
+  }, [])
+
+  return (
+    <div>
+    <h2>Posts</h2>
+    <ul>
+        {/* Posts go here! Include link to see post with comments */}
+    </ul>
+    <hr />
+    <h2>Users</h2>
+    <ul>
+        {users.map(user => (
+        <li key={user.id}>
+            <span>
+            {user.username}, age: {user.age}, email: {user.email}
+            </span>
+        </li>
+    ))}
+    </ul>
+    <hr />
+    <SignUpForm />
+    </div>
+  )
 }
 
 export default Home;
