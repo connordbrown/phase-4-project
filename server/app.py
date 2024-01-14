b00eae19a9ffdcc5abf227e2089a48ad61be3dfd
Connor <connordbrown@gmail.com>
2024-01-14 15:17:31 -0700

    add User get method to app

--- server/app.py
+++ server/app.py
@@ -1,9 +1,20 @@
+from flask import request, session, make_response
+from flask_restful import Resource
+from sqlalchemy.exc import IntegrityError
+
 from config import app, db, api
 from models import User
 
 @app.route('/')
 def home():
-  return 'Welcome to Post Maker!'
+    return 'Welcome to Post Maker!'
+
+class Users(Resource):
+    def get(self):
+      users_dict_list = [u.to_dict() for u in User.query.all()]
+      return make_response(users_dict_list, 200)
+  
+api.add_resource(Users, '/users')
 
 if __name__ == "__main__":
-  app.run(port=5555, debug=True)
+    app.run(port=5555, debug=True)
