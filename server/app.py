2c6bbfe36568df8cd7f8036f0ff9301c35a2f234
Connor <connordbrown@gmail.com>
2024-01-14 15:29:46 -0700

    add post method to Users

--- server/app.py
+++ server/app.py
@@ -13,6 +13,37 @@ class Users(Resource):
     def get(self):
       users_dict_list = [u.to_dict() for u in User.query.all()]
       return make_response(users_dict_list, 200)
+    
+    def post(self):
+        username = request.json.get('username')
+        age = request.json.get('age')
+        email = request.json.get('email')
+        password = request.json.get('password')
+
+        if not username or not age or not email or not password:
+            return make_response({'errors': ['validation errors']}, 400)
+        if not (10 <= int(age) <= 120):
+            return make_response({'errors': ['validation errors']}, 400)
+        if ('@' or '.') not in email:
+            return make_response({'errors': ['validation errors']}, 400)
+        
+        new_user = User(
+            username=username,
+            age=age,
+            email=email
+        )
+
+        # password encrypted by setter
+        new_user._password_hash = password
+
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+            # get user id with data
+            new_user_data = db.session.get(User, new_user.id)
+            return make_response(new_user_data.to_dict(), 201)
+        except IntegrityError:
+            return make_response({'error': '422: Unprocessable Entity'}, 422)
   
 api.add_resource(Users, '/users')
 
